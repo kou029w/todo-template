d671630123b746bca8759e4d41a0c110fcda6349
Kohei Watanabe <nebel@fogtype.com>
2026-02-05 04:41:15 +0900

    build(web): update TypeScript configuration and optimize HTML entry point handling in Vite

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,16 +3,20 @@ import react from "@vitejs/plugin-react";
 import fs from "node:fs/promises";
 import { defineConfig } from "vite";
 
+const entrypoints: string[] = [];
+for await (const html of fs.glob("**/*.html", {
+  cwd: import.meta.dirname,
+  exclude: ["node_modules/**", "dist/**"],
+})) {
+  entrypoints.push(html);
+}
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
-      input: await Array.fromAsync(
-        fs.glob("**/*.html", {
-          exclude: ["node_modules/**", "dist/**"],
-        }),
-      ),
+      input: entrypoints,
     },
   },
 });
