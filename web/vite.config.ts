5d3a216460ff338c64b1794a11793b1276dc50fb
Kohei Watanabe <nebel@fogtype.com>
2026-02-05 04:18:51 +0900

    build(web): include all HTML entry points in Vite build

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,8 +1,18 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import fs from "node:fs/promises";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: await Array.fromAsync(
+        fs.glob("**/*.html", {
+          exclude: ["node_modules/**", "dist/**"],
+        }),
+      ),
+    },
+  },
 });
